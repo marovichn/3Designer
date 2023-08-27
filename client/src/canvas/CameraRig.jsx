f5a52a881b89c9484f54e537e0fa5653faa7dc15
marovichn <nikolamarovic.11@gmail.com>
2023-08-27 22:45:12 +0200

    Positioning 3D Model across devices

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
 
-const CameraRig = () => {
-  return (
-    <div>CameraRig</div>
-  )
-}
+import state from "../store";
 
-export default CameraRig
+const CameraRig = ({ children }) => {
+  const snap = useSnapshot(state);
+  const group = useRef();
+
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    //initial position
+    let targetPosition = [-0.4, 0, 0.7];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0.2, 1];
+      if (isMobile) targetPosition = [0, 0.35, 1.1];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 1];
+      else targetPosition = [0, 0, 0.7];
+    }
+
+    //set camera poisition
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
+};
+
+export default CameraRig;
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Canvas } from "@react-three/fiber"
-import { Environment, Center } from "@react-three/drei"
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-import Backdrop from './Backdrop';
-import Shirt from './Shirt';
-import CameraRig from './CameraRig';
+import Backdrop from "./Backdrop";
+import Shirt from "./Shirt";
+import CameraRig from "./CameraRig";
 
 const CanvasModel = () => {
   return (
     <Canvas>
-      <ambientLight intensity={0.5}/>
-      <Environment preset='city'/>
-      {/* <CameraRig>
-        <Backdrop/> */}
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+      <CameraRig>
+        {/* <Backdrop/> */}
         <Center>
-          <Shirt/>
+          <Shirt />
         </Center>
-      {/* </CameraRig> */}
+      </CameraRig>
     </Canvas>
-  )
-}
+  );
+};
 
-export default CanvasModel
+export default CanvasModel;
